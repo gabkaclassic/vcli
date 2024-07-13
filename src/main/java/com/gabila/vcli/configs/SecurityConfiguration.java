38fb288de9cf4c88cb43471ff77a32316f9f7260
Errodion <r.kuzmin.w@gmail.com>
2024-07-13 18:23:36 +0300

    feat(Configurations): Security configuration
    Security configuration for developing added

---
+++ src/main/java/com/gabila/vcli/configs/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.gabila.vcli.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+        ;
+
+        return http.build();
+    }
+}
